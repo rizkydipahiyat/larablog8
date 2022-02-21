8b621b47067a55c75599e4c356e670788891f283
rizkydipahiyat <60695993+rizkydipahiyat@users.noreply.github.com>
2022-02-21 16:29:08 +0700

    Refaktorisasi validate insert kategori

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 /** php artisan make:controller CategoryController --resource --model=Category
  * perintah diatas untuk membuat CRUD dan auto import models category 
@@ -54,14 +55,22 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        $request->validate([
+        $validator = Validator::make($request->all(), [
             'title' => 'required|string|max:60',
             'slug' => 'required|string|unique:categories,slug',
             'thumbnail' => 'required',
             'description' => 'required|string|max:240',
         ]);
 
-        dd($request->all());
+        if ($validator->fails()) {
+            if ($request->has('parent_category')) {
+                $request['parent_category'] = Category::select('id', 'title')->find($request->parent_category);
+            }
+            return redirect()->back()->withInput($request->all())->withErrors($validator);
+        }
+
+        // proses insert data
+        dd("proses insert berhasil", $request->all());
     }
 
     /**
